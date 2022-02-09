8e084f7503759f4ddd3ec1438d80f8ae3596306e
Angus McRitchie <angus@hobbiesdirect.com.au>
2022-02-09 18:22:02 +1100

    Update to use @gooby/string

--- index.js
+++ index.js
@@ -1,26 +1,18 @@
-function convert(input, convertFn) {
+import string from '@gooby/string';
+
+function convertKeys(input, convertFn) {
     if (Array.isArray(input)) {
-        return input.map((item) => convert(item, convertFn));
+        return input.map((item) => convertKeys(item, convertFn));
     }
 
     if (!input || typeof input !== 'object' || Object.getPrototypeOf(input).constructor !== Object) {
         return input;
     }
 
-    return Object.keys(input).reduce((acc, key) => ({...acc, [convertFn(key)]: convert(input[key], convertFn)}), {});
+    return Object.keys(input).reduce((acc, key) => ({...acc, [convertFn(key)]: convertKeys(input[key], convertFn)}), {});
 }
 
-function stringToCamel(string) {
-    return string.replace(/([-_/][a-z])/gi, ($1) => $1.toUpperCase().replace(/\/|-|_/g, ''))
-}
+export const toCamel = (input) => convertKeys(input, string.toCamel);
+export const toSnake = (input) => convertKeys(input, string.toSnake);
 
-function stringToSnake(string) {
-    return string.replace(/[A-Z]/g, letter => `_${letter.toLowerCase()}`).replace(/\/|-/g, '_');
-}
-
-const keys = {
-    toCamel: (input) => convert(input, stringToCamel),
-    toSnake: (input) => convert(input, stringToSnake),
-}
-
-export default keys;
+export default {toCamel, toSnake};
